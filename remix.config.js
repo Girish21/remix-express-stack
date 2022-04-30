79fd647f351615c5ca799ed92d1f423ba03778ed
Girish21 <girishkumarv21@gmail.com>
2022-04-30 16:02:41 +0530

    chore: compile chalk to cjs for server

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  serverDependenciesToBundle: [/chalk/i, '#ansi-styles', '#supports-color'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
